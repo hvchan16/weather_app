745a04ea8b5afff93416ad0d370d5c03c14a9fd6
Harshvardhan Chauhan <harshvardhancc@gmail.com>
2021-08-17 20:48:04 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, render_template, Response, redirect
+import requests
+import configparser
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about ')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    zip_code = request.form['zipCode']
+
+    api_key = get_api_key()
+    data = get_weather_results(zip_code, api_key)
+    temp_c = data["main"]["temp"] - 273
+    temp = "{0:.2f}".format(temp_c)
+    feels_like_c = data["main"]["feels_like"] - 273
+    feels_like = "{0:.2f}".format(feels_like_c)
+    weather = data["weather"][0]["main"]
+    location = data["name"]
+
+    return render_template('weather.html',
+                           location=location, temp=temp,
+                           feels_like=feels_like, weather=weather, flag="wflag")
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api']
+
+
+def get_weather_results(zip_code, api_key):
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    api_url = base_url + "appid=" + api_key + "&q=" + zip_code
+    r = requests.get(api_url)
+    return r.json()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8500)
